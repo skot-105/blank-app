de4fd47076d47c71dc43663134bb7f2a1848a20d
skot-105 <skkotian@ncsu.edu>
2024-08-26 16:25:25 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,15 +1,18 @@
 import streamlit as st
 
+# Page configuration
 st.set_page_config(page_title="Video Annotation App", layout="wide")
+
+# Main Page Content
 st.title("Video Uploader")
 
-col1 = st.columns(1)
-with col1:
-    st.subheader("Upload Video")
-    uploaded_video = st.file_uploader("Drag and drop a video file here or click", type=["mp4", "mov", "avi"])
-    
-    if uploaded_video is not None:
-        st.video(uploaded_video)
+# Single video uploader
+st.subheader("Upload Video")
+uploaded_video = st.file_uploader("Drag and drop a video file here or click", type=["mp4", "mov", "avi"])
 
+if uploaded_video is not None:
+    st.video(uploaded_video)
+
+# Upload Button
 if st.button('Upload'):
-    st.write("Files uploaded successfully!")
+    st.write("Video uploaded successfully!")
